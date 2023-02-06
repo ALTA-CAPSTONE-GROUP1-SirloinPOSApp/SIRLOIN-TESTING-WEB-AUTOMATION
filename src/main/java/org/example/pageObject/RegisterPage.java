00de3846676704142c5edd50edaa6a1c66bb10ce
Mukhlis Anshori Witanto <mukhlisanshori1997@gmail.com>
2023-02-06 22:07:27 +0700

    Register Done

--- src/main/java/org/example/pageObject/RegisterPage.java
+++ src/main/java/org/example/pageObject/RegisterPage.java
@@ -95,9 +95,40 @@ public class RegisterPage {
         System.out.println("Button is enabled ? "+registerUser.isEnabled());
     }
 
-    @FindBy(id = "swal2-title")
+    @FindBy(xpath = "//div[@class='swal2-container swal2-center swal2-backdrop-show']")
     private WebElement modal;
-    public String popModal(){
-        return modal.getText();
+    public boolean popModal(){
+        modal.isDisplayed();
+        return true;
+    }
+
+    @FindBy(xpath = "//h2[@class='swal2-title']")
+    private WebElement judulModal ;
+    public String getJudulModal(){
+        return judulModal.getText();
+    }
+
+    @FindBy(xpath = "//div[@class='swal2-html-container']")
+    private WebElement messageModal ;
+    public String getPesanModal(){
+        return messageModal.getText();
+    }
+
+    @FindBy(xpath = "//button[@class='swal2-confirm swal2-styled swal2-default-outline']")
+    private WebElement clickModalOk;
+    public void setClickModalOk(){
+        clickModalOk.click();
+    }
+
+    @FindBy(xpath = "//p[@id='to-login']")
+    private WebElement loginOnRegister ;
+    public void clickLoginOnRegister(){
+        loginOnRegister.click();
+    }
+
+    @FindBy(xpath ="//h1[@class='text-center text-[#306D75] font-bold text-5xl mb-11']")
+    private WebElement titleLogin ;
+    public String getTitleLogin(){
+        return titleLogin.getText();
     }
 }
