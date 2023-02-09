26eb0c78c57144db36cf6adeb988308a44ed4cff
Mukhlis Anshori Witanto <mukhlisanshori1997@gmail.com>
2023-02-09 21:45:06 +0700

    update laporan transaksi

--- src/main/java/org/example/pageObject/LaporanTransaksiPage.java
+++ src/main/java/org/example/pageObject/LaporanTransaksiPage.java
@@ -22,4 +22,31 @@ public class LaporanTransaksiPage {
     public String getJudulLaporan(){
         return judulLaporan.getText();
     }
+    @FindBy(xpath = "//input[@id='start-date']")
+    private WebElement tanggalMulai ;
+    public void clickTanggalMulai(){
+        tanggalMulai.click();
+    }
+    @FindBy(xpath = "//div[@class='react-datepicker__day react-datepicker__day--001']")
+    private WebElement pilihTanggalMulai ;
+    public void pilihTanggalMulai(){
+        pilihTanggalMulai.click();
+    }
+    @FindBy(xpath = "//input[@id='end-date']")
+    private WebElement tanggalSampai;
+    public void clickTanggalSampai(){
+        tanggalSampai.click();
+    }
+    @FindBy(xpath = "//div[@class='react-datepicker__day react-datepicker__day--028']")
+    private WebElement pilihTanggalSampai;
+    public void setPilihTanggalSampai(){
+        pilihTanggalSampai.click();
+    }
+    @FindBy(xpath = "//button[@id='tampil-data']")
+    private WebElement tampilData;
+    public void clickTampilData(){
+        tampilData.click();
+    }
+
+
 }
